85a25b00dbf9692b091e040160c4d0ac44401f70
IzzyHolty <Izzyholty@gmail.com>
2019-03-29 00:06:20 +1100

    Fixed validating input loop bug and added functions

--- RevisedJulianCalendarLeapYear.py
+++ RevisedJulianCalendarLeapYear.py
@@ -5,15 +5,28 @@ import random
 import sys
 import os
 
-#validating user input
-try:
-    year = int(input("What year would you like to test? "))
-except ValueError:
-    print("That's not a year!")
-    year = int(input("What year would you like to test? "))
-else:
+#leap year checking function
+def isleapyear(int):
     if ((year % 4 == 0 and year % 100 != 0) or (year % 100 == 0 and year % 400 == 0) or (year % 900 == 200 or 600)) :
         print(year, "is a leap year.")
+        return True
     else :
         print(year, "is not a leap year.")
+        return False
+
+#validating user input
+
+while True:
+    try:
+        year = int(input("What year would you like to test? "))
+    except ValueError:
+        print("That's not a year!")
+        continue
+    else:
+        break
+
+yearcheck = isleapyear(year)
+
+
+
 
