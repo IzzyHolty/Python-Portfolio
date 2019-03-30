712875f10a0d4e1065b180de40b7bde3b071c8ca
IzzyHolty <Izzyholty@gmail.com>
2019-03-31 10:48:23 +1100

    Putting in functions

--- PumpkinKing.py
+++ PumpkinKing.py
@@ -4,36 +4,47 @@ import os
 import pygame
 pygame.init()
 
-
-window = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("The Pumpkin King")
+#Global Variables
+screen_width = 500
+screen_height = 500
 
 character_x = 50
-character_y = 420
+character_y = 440
 width = 40
 height = 60
-velocity = 5
+velocity = 10
+boundary = 13
 
-run = True
-while run:
-    pygame.time.delay(100)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
+def keyPress():
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and character_x > boundary:
         character_x -= velocity
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and character_x < screen_width + boundary:
         character_x += velocity
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and character_y > boundary:
         character_y-= velocity
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and character_y < screen_height + boundary:
         character_y += velocity
 
-    window.fill((0,0,1))
-    pygame.draw.circle(window, (pygame.Color('#e58124')), (character_x, character_y), 50, 50)
-    pygame.display.update()
+def drawGame():
+    window = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("The Pumpkin King")
 
-pygame.quit()
+    run = True
+    while run:
+        pygame.time.delay(80)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        keypress()
+
+        window.fill((0,0,1))
+        pygame.draw.circle(window, (pygame.Color('#e58124')), (character_x, character_y), 50, 50)
+        pygame.display.update()
+
+    pygame.quit()
+
+#Main
+drawGame()
