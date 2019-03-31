8539a46a419580ef294f9dbfec98706acfb87e2e
IzzyHolty <Izzyholty@gmail.com>
2019-03-31 11:29:22 +1100

    Working on jump

--- PumpkinKing.py
+++ PumpkinKing.py
@@ -4,59 +4,57 @@ import os
 import pygame
 pygame.init()
 
-#Global Variables
+# Global Variables
 screen_width = 500
 screen_height = 500
 character_x = 50
 character_y = 450
+boundary = 13
+width = 40
+height = 60
+velocity = 10
+isJump = False
+jumpCount = 10
 
 
-def keyPress(x_pos, y_pos, scr_width, scr_height):
-    boundary = 13
-    isJump = False
-    jumpCount = 10
-    width = 40
-    height = 60
-    velocity = 10
+
+window = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("The Pumpkin King")
+
+run = True
+while run:
+    pygame.time.delay(80)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x_pos > boundary:
-        x_pos -= velocity
-    if keys[pygame.K_RIGHT] and x_pos < scr_width + boundary:
-        x_pos += velocity
+    if keys[pygame.K_LEFT] and character_x > boundary:
+        character_x -= velocity
+    if keys[pygame.K_RIGHT] and character_x < screen_width + boundary:
+        character_x += velocity
     if not (isJump):
-        if keys[pygame.K_UP] and y_pos> boundary:
-            y_pos-= velocity
-        if keys[pygame.K_DOWN] and y_pos < scr_height + boundary:
-            y_pos += velocity
+        if keys[pygame.K_UP] and character_y > boundary:
+            character_y -= velocity
+        if keys[pygame.K_DOWN] and character_y < screen_height + boundary:
+            character_y += velocity
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
         if jumpCount >= -10:
-            y_pos -= (jumpCount ** 2) * 0.5
+            neg = 1
+            if jumpCount < 10:
+                neg = -1
+            character_y -= (jumpCount ** 2) * 1 * neg
             jumpCount -= 1
         else:
             isJump = False
             jumpCount = 10
 
-def drawGame():
-    window = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("The Pumpkin King")
+    window.fill((0, 0, 1))
+    pygame.draw.circle(window, pygame.Color('#e58124'), (character_x, character_y), 50, 50)
+    pygame.display.update()
 
-    run = True
-    while run:
-        pygame.time.delay(80)
+pygame.quit()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-        keyPress(character_x, character_y, screen_width, screen_height)
-
-        window.fill((0,0,1))
-        pygame.draw.circle(window, (pygame.Color('#e58124')), (character_x, character_y), 50, 50)
-        pygame.display.update()
-
-    pygame.quit()
-
-#Main
-drawGame()
