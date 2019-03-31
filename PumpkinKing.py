a0256223ee6e30689accda9e241f6977a197778b
IzzyHolty <Izzyholty@gmail.com>
2019-03-31 11:06:06 +1100

    Fixing scopes

--- PumpkinKing.py
+++ PumpkinKing.py
@@ -7,32 +7,33 @@ pygame.init()
 #Global Variables
 screen_width = 500
 screen_height = 500
-
 character_x = 50
-character_y = 440
-width = 40
-height = 60
-velocity = 10
-boundary = 13
-isJump = False
-jumpCount = 10
+character_y = 450
 
-def keyPress():
+
+def keyPress(x_pos, y_pos, scr_width, scr_height):
+    boundary = 13
+    isJump = False
+    jumpCount = 10
+    width = 40
+    height = 60
+    velocity = 10
+
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and character_x > boundary:
-        character_x -= velocity
-    if keys[pygame.K_RIGHT] and character_x < screen_width + boundary:
-        character_x += velocity
+    if keys[pygame.K_LEFT] and x_pos > boundary:
+        x_pos -= velocity
+    if keys[pygame.K_RIGHT] and x_pos < scr_width + boundary:
+        x_pos += velocity
     if not (isJump):
-        if keys[pygame.K_UP] and character_y > boundary:
-            character_y-= velocity
-        if keys[pygame.K_DOWN] and character_y < screen_height + boundary:
-            character_y += velocity
+        if keys[pygame.K_UP] and y_pos> boundary:
+            y_pos-= velocity
+        if keys[pygame.K_DOWN] and y_pos < scr_height + boundary:
+            y_pos += velocity
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
         if jumpCount >= -10:
-            character_y -= (jumpCount ** 2) * 0.5
+            y_pos -= (jumpCount ** 2) * 0.5
             jumpCount -= 1
         else:
             isJump = False
@@ -49,7 +50,7 @@ def drawGame():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        keyPress()
+        keyPress(character_x, character_y, screen_width, screen_height)
 
         window.fill((0,0,1))
         pygame.draw.circle(window, (pygame.Color('#e58124')), (character_x, character_y), 50, 50)
