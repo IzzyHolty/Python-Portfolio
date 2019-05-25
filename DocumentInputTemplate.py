6edd8d1b843909ce8cac0b75635d0069b7aac99d
IzzyHolty <Izzyholty@gmail.com>
2019-05-25 22:08:51 +1000

    edits

--- DocumentInputTemplate.py
+++ DocumentInputTemplate.py
@@ -10,6 +10,19 @@ import string
 from tkinter import *
 
 
+#text above entry box
+def textEntry(words, xpos, ypos):
+    textlabel = Label(root, text=words, font=("Times New Roman", 11))
+    textlabel.place(x=xpos, y=ypos)
+
+#entry field
+def entryBox(xpos, ypos):
+    entryName = Entry(root, width=20, bg="lightgray")
+    entryName.place(x=xpos, y=ypos)
+    userinput = entryName.get()
+    return userinput
+
+
 #create document
 reportdoc = docx.Document()
 
@@ -22,27 +35,27 @@ root.title("Document Generator")
 heading = Label(root, text="Create a Document Template", font=("Times New Roman", 20, "bold"))
 heading.pack(side=TOP)
 
-#Form fields
-entryName = Entry(root, width=35, bg="lightgray")
-entryName.pack()
-entryName.place(x=35, y=80)
-requestName = entryName.get()
+textEntry("The person's name is", 35, 60)
+requestName = entryBox(35, 80)
 
-#write name with text cocatenated to doc
+#write name with text concatenated to doc
 firstParagraph = reportdoc.add_paragraph("This person's name is " + requestName + ".")
 
-thingLike = Entry(root, width=35, bg="lightgray")
-thingLike.pack()
-thingLike.place(x=35, y=100)
+#text above entry box
+textlabel = Label(root, text="They like", font=("Times New Roman", 11))
+textlabel.place(x=35, y=120)
+
+#enter person's thing they like
+thingLike = Entry(root, width=20, bg="lightgray")
+thingLike.place(x=35, y=140)
 personLikes = thingLike.get()
 
 #continue on line
 firstParagraph.add_run("They like " + personLikes + ".")
 
 #Name the document
-docName = Entry(root, width=35, bg="lightgray")
-docName.pack()
-docName.place(x=35, y=80)
+docName = Entry(root, width=20, bg="lightgray")
+docName.place(x=35, y=160)
 documentname = docName.get()
 
 #submit button
@@ -50,8 +63,7 @@ def submitButtonFunction():
 	reportdoc.save(documentname + ".docx")
 
 submitButton = Button(root, text="Submit", command=submitButtonFunction())
-submitButton.pack()
-submitButton.place(x=35, y=120)
+submitButton.place(x=35, y=180)
 
 #main
 root.mainloop()
